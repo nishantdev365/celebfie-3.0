8abb93d2a2838cf1686fe2da9de1a808f2d52b03
nishantdev365 <nishantk322@gmail.com>
2024-01-16 01:20:06 +0530

    bugs fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
-import Intro from "./pages/introPage"
+import Intro from "./pages/introPage";
 import Home from "./pages/home";
 import ChatPage from "./pages/chatPage";
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-      <Navbar />
+      <BrowserRouter basename="/nishantdev365">
+        <Navbar />
         <Routes>
           <Route path="/" element={<Intro />} />
           <Route path="/home" element={<Home />} />
