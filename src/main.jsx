0ddf1a6991b2833007fb45077735b7daec0cc7d8
nishantdev365 <nishantk322@gmail.com>
2024-01-21 20:18:29 +0530

    Update for domain assignment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+// import React from 'react';
+import { hydrateRoot, createRoot } from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(<App />, rootElement);
+} else {
+  createRoot(rootElement).render(<App />);
+}
