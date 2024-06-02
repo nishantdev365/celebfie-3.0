0bba473ed349c6af51ae2cf2d49873d7b064c5df
nishantdev365 <nishantk322@gmail.com>
2024-06-02 21:23:50 +0530

    about page update

--- src/pages/aboutUs.jsx
+++ src/pages/aboutUs.jsx
@@ -1,6 +1,4 @@
-// import HeroImg from '../assets/slide-img2.jpg'
-// import WorksImg from "../assets/workimg.png";
-import ExplainImg from "../assets/explain_img.png";
+
 import QuoteImg from "../assets/quote.png";
 import RaminderSingh from "../assets/raminder_singh.png";
 import TeamProfile1 from "../assets/team_profile1.png";
@@ -8,9 +6,27 @@ import TeamProfile2 from "../assets/team_profile2.png";
 import TeamProfile3 from "../assets/team_profile3.png";
 import TeamProfile4 from "../assets/team_profile4.png";
 import TeamProfile5 from "../assets/team_profile5.png";
-import Img1 from "../assets/image1.png"
-import Img2 from "../assets/image2.png"
-import Img3 from "../assets/image3.png"
+import Img1 from "../assets/image1.png";
+import Img2 from "../assets/image2.png";
+import Img3 from "../assets/image3.png";
+import ArrowRight from "../assets/arrow-right.png";
+import ArrowLeft from "../assets/arrow-left.png";
+import AboutImg1 from "../assets/about-page-img1.png";
+import AboutImg2 from "../assets/about-page-img2.png";
+import AboutImg3 from "../assets/about-page-img3.png";
+
+import TestimonialPic1 from "../assets/testimonialpic1.png";
+import TestimonialPic2 from "../assets/testimonialpic2.png";
+import TestimonialPic3 from "../assets/testimonialpic3.png";
+import TestimonialPic4 from "../assets/testimonialpic4.png";
+import TestimonialPic5 from "../assets/testimonialpic5.png";
+import TestimonialPic6 from "../assets/testimonialpic6.png";
+import TestimonialPic7 from "../assets/testimonialpic7.png";
+import TestimonialPic8 from "../assets/testimonialpic8.png";
+import TestimonialPic9 from "../assets/testimonialpic9.png";
+import TestimonialPic10 from "../assets/testimonialpic10.png";
+import TestimonialPic11 from "../assets/testimonialpic11.png";
+import TestimonialPic12 from "../assets/testimonialpic12.png";
 
 import Logo from "../assets/logo.svg";
 import Facebook from "../assets/facebook_icon.png";
@@ -18,8 +34,96 @@ import Instagram from "../assets/instagram_icon.png";
 import Visa from "../assets/visa-mastercard-icon.png";
 import Facebook_Vector from "../assets/facebook_vector.png"
 import Instagram_Vector from "../assets/instagram_vector.png"
+import { useState } from 'react';
+
+const testimonials = [
+  {
+    id: 1,
+    name: 'Jane Doe',
+    image: TestimonialPic1,
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sodales, turpis sed sodales ullamcorper, ligula urna cursus augue, a ullamcorper est nulla eu magna. Maecenas lacus lorem, cursus non.',
+  },
+  {
+    id: 2,
+    name: 'John Smith',
+    image: TestimonialPic2,
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sodales, turpis sed sodales ullamcorper, ligula urna cursus augue, a ullamcorper est nulla eu magna. Maecenas lacus lorem, cursus non.',
+  },
+  {
+    id: 3,
+    name: 'John Smith',
+    image: TestimonialPic3,
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sodales, turpis sed sodales ullamcorper, ligula urna cursus augue, a ullamcorper est nulla eu magna. Maecenas lacus lorem, cursus non.',
+  },
+  {
+    id: 4,
+    name: 'John Smith',
+    image: TestimonialPic4,
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sodales, turpis sed sodales ullamcorper, ligula urna cursus augue, a ullamcorper est nulla eu magna. Maecenas lacus lorem, cursus non.',
+  },
+  {
+    id: 5,
+    name: 'John Smith',
+    image: TestimonialPic5,
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sodales, turpis sed sodales ullamcorper, ligula urna cursus augue, a ullamcorper est nulla eu magna. Maecenas lacus lorem, cursus non.',
+  },
+  {
+    id: 6,
+    name: 'John Smith',
+    image: TestimonialPic6,
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sodales, turpis sed sodales ullamcorper, ligula urna cursus augue, a ullamcorper est nulla eu magna. Maecenas lacus lorem, cursus non.',
+  },
+  {
+    id: 7,
+    name: 'John Smith',
+    image: TestimonialPic7,
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sodales, turpis sed sodales ullamcorper, ligula urna cursus augue, a ullamcorper est nulla eu magna. Maecenas lacus lorem, cursus non.',
+  },
+  {
+    id: 8,
+    name: 'John Smith',
+    image: TestimonialPic8,
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sodales, turpis sed sodales ullamcorper, ligula urna cursus augue, a ullamcorper est nulla eu magna. Maecenas lacus lorem, cursus non.',
+  },
+  {
+    id: 9,
+    name: 'John Smith',
+    image: TestimonialPic9,
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sodales, turpis sed sodales ullamcorper, ligula urna cursus augue, a ullamcorper est nulla eu magna. Maecenas lacus lorem, cursus non.',
+  },
+  {
+    id: 10,
+    name: 'John Smith',
+    image: TestimonialPic10,
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sodales, turpis sed sodales ullamcorper, ligula urna cursus augue, a ullamcorper est nulla eu magna. Maecenas lacus lorem, cursus non.',
+  },
+  {
+    id: 11,
+    name: 'John Smith',
+    image: TestimonialPic11,
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sodales, turpis sed sodales ullamcorper, ligula urna cursus augue, a ullamcorper est nulla eu magna. Maecenas lacus lorem, cursus non.',
+  },
+  {
+    id: 12,
+    name: '12',
+    image: TestimonialPic12,
+    quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sodales, turpis sed sodales ullamcorper, ligula urna cursus augue, a ullamcorper est nulla eu magna. Maecenas lacus lorem, cursus non.',
+  },
+];
 
 const aboutUs = () => {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [hoverIndex, setHoverIndex] = useState(null);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonials.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + testimonials.length) % testimonials.length);
+  };
+
   return (
     <>
       <main>
@@ -73,10 +177,10 @@ const aboutUs = () => {
               </p>
             </div>
             <div className="image2">
-              <img src={ExplainImg} alt="" />
+              <img src={AboutImg1} alt="" />
             </div>
           </div>
-          <hr className="hr_line"/>
+         
           <div className="explain_content2">
             <div className="content2">
               <h1 style={{ fontSize: "45px", fontWeight: "700" }}>
@@ -94,31 +198,102 @@ const aboutUs = () => {
               </p>
             </div>
             <div className="image2">
-              <img src={ExplainImg} alt="" />
+              <img src={AboutImg2} alt="" />
             </div>
           </div>
-        </div>
 
-        <div className="bottom_section">
-          <div className="explain_content3">
+          <div className="explain_content1">
             <div className="content2">
-              <p style={{ fontSize: "34px", lineHeight: "46px" }}>
-                <span>
-                  <img src={QuoteImg} alt="" />
-                </span>
+              <h1 style={{ fontSize: "45px", fontWeight: "700" }}>
+                Explain how this benefits for brands
+              </h1>
+              <p style={{ fontSize: "16px", lineHeight: "27px" }}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus sodales, turpis sed sodales ullamcorper, ligula urna
-                cursus augue, a ullamcorper est nulla eu magna. Maecenas lacus
-                lorem, cursus non.
-              </p>
-              <p style={{ fontSize: "27px", lineHeight: "46px" }}>
-                . Raminder Singh
+                Suspendisse at porttitor mi. In eu ornare enim, vitae tincidunt
+                magna. Nullam at convallis diam. Cras vitae tortor justo. Donec
+                varius justo rhoncus, luctus est et, luctus erat. Etiam rutrum
+                vel ipsum vel ultricies. Quisque efficitur ut purus vitae
+                tristique. Proin nisl velit, gravida non elementum ut, suscipit
+                at turpis. Aenean viverra ex massa, et interdum lectus efficitur
+                in.
               </p>
             </div>
             <div className="image2">
-              <img src={RaminderSingh} alt="" />
+              <img src={AboutImg3} alt="" />
             </div>
           </div>
+
+          <hr className="hr_line"/>
+
+
+
+          <div className="testimonial_container">
+      <div className="testimonial_title">
+      <p className="title_text">Some of our Happy Fans</p>
+
+      <div className="arrow_icons">
+     
+        <img onClick={handlePrev} src={ArrowLeft} alt="" />
+        <img onClick={handleNext} src={ArrowRight} alt="" />
+     
+
+      </div>
+
+      </div>
+      <div className="fan_profile_pic_section">
+      {/* {testimonials.map((testimonial, index) => (
+        <img
+          key={testimonial.id}
+          src={testimonial.image}
+          alt={testimonial.name}
+          className={`fan_profile_pic ${index === currentIndex ? 'active' : ''} ${index === hoverIndex ? 'hover' : ''}`}
+          onClick={() => setCurrentIndex(index)}
+          onMouseEnter={() => setHoverIndex(index)}
+          onMouseLeave={() => setHoverIndex(null)}
+        />
+      ))} */}
+
+{testimonials.map((testimonial, index) => (
+    <div key={testimonial.id} className="profile_container">
+      <img
+        src={testimonial.image}
+        alt={testimonial.name}
+        className={`fan_profile_pic ${index === currentIndex ? 'active' : ''} ${index === hoverIndex ? 'hover' : ''}`}
+        onClick={() => setCurrentIndex(index)}
+        onMouseEnter={() => setHoverIndex(index)}
+        onMouseLeave={() => setHoverIndex(null)}
+      />
+      {index === currentIndex && <div className="active_dot"></div>}
+    </div>
+  ))}
+      </div>
+
+
+      <div className="testimonial_content">
+        <img
+          src={testimonials[currentIndex].image}
+          alt={testimonials[currentIndex].name}
+          className="testimonial_image"
+        />
+       <div className="quote_and_fan_name">
+      
+        <img style={{"width":"40px", "height":"40px" }} src={QuoteImg} alt="" />
+               
+       <p className="quote">{testimonials[currentIndex].quote}</p>
+        <p className="author">- {testimonials[currentIndex].name}</p>
+       </div>
+      </div>
+
+
+
+     
+    </div>
+        </div>
+
+      
+
+        <div className="bottom_section">
+         
 
           <p
          
@@ -184,6 +359,27 @@ const aboutUs = () => {
                   </div>
                 </div>
               </div>
+            </div>
+          </div>
+
+
+          <div className="explain_content3">
+            <div className="content2">
+              <p style={{ fontSize: "34px", lineHeight: "46px" }}>
+                <span>
+                  <img src={QuoteImg} alt="" />
+                </span>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Phasellus sodales, turpis sed sodales ullamcorper, ligula urna
+                cursus augue, a ullamcorper est nulla eu magna. Maecenas lacus
+                lorem, cursus non.
+              </p>
+              <p style={{ fontSize: "27px", lineHeight: "46px" }}>
+                . Raminder Singh
+              </p>
+            </div>
+            <div className="image2">
+              <img src={RaminderSingh} alt="" />
             </div>
           </div>
         </div>
